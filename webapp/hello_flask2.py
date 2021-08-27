e9df9fdd9fa6d52f3362232b2b7549dbae407ea5
michaljasinski028@gmail.com <michaljasinski028@gmail.com>
2021-08-27 14:23:17 +0200

    modified webapp

--- webapp/hello_flask2.py
+++ webapp/hello_flask2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -7,15 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello() -> str:
-   return 'Witaj, świecie, tu Flask!'
+   return 'Welcome on lab'
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
-   return str(search4letters('życie, wszechświat i cała reszta', 'eiru,!'))
+   phrase = request.form['phrase']
+   letters = request.form['letters']
+   title = 'Result:'
+   results = str(search4letters(phrase, letters))
+   return render_template('results.html',
+                               the_phrase=phrase,
+                               the_letters=letters,
+                               the_title=title,
+                               the_results=results,)
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
                              the_title='Welcome on website')
-app.run()
+app.run(debug=True)
