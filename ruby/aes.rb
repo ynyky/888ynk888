2dce884f7ec75bbfba720f4993535929388bf94e
Michal Jasinski <michal.jasinski@conceptboard.com>
2024-09-25 12:33:28 +0000

    optimimization

--- ruby/aes.rb
+++ ruby/aes.rb
@@ -7,15 +7,14 @@ def generate_random_password(length = 12)
   password = Array.new(length) { characters.sample }.join
   password
 end
-def generate_aes128_key(password)
+def generate_aes128_key
   key = OpenSSL::Random.random_bytes(16) 
   base64_key = Base64.strict_encode64(key)
   { key: base64_key }
 end
 
 # Example usage
-password = generate_random_password
-key_info = generate_aes128_key(password)
+key_info = generate_aes128_key
 
 puts "Key: #{key_info[:key]}"
 
