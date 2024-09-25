92b0128508047d2ea629b615379323835c139e1e
Michal Jasinski <michal.jasinski@conceptboard.com>
2024-09-25 12:30:02 +0000

    generate aes in ruby

---
+++ ruby/aes.rb
@@ -0,0 +1,22 @@
+require 'openssl'
+require 'base64'
+require 'securerandom'
+
+def generate_random_password(length = 12)
+  characters = ('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a + ['@', '#', '$', '%', '&', '*']
+  password = Array.new(length) { characters.sample }.join
+  password
+end
+def generate_aes128_key(password)
+  key = OpenSSL::Random.random_bytes(16) 
+  base64_key = Base64.strict_encode64(key)
+  { key: base64_key }
+end
+
+# Example usage
+password = generate_random_password
+key_info = generate_aes128_key(password)
+
+puts "Key: #{key_info[:key]}"
+
+
